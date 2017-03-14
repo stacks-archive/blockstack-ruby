d47eddb0eb6333997ed413889b0e9cbb504c6ca8
Larry Salibra <larry@appartisan.com>
2017-03-15 01:17:44 +0800

    verify authentication token

--- lib/blockstack.rb
+++ lib/blockstack.rb
@@ -1,9 +1,58 @@
 require "blockstack/version"
-require 'bitcoin'
-require 'faraday'
+require "bitcoin"
+require "faraday"
+require "jwt"
 
 module Blockstack
     USER_AGENT = "blockstack-ruby #{VERSION}"
+    ALGORITHM = "ES256K"
+    REQUIRED_CLAIMS = %w(iss iat jti exp username profile publicKeys)
+    LEEWAY = 300 # seconds
+    VALID_WITHIN = 300 # seconds
+    BLOCKSTACK_API = "http://localhost:6270"
+
+    class InvalidAuthResponse < StandardError; end
+
+    def self.verify_auth_response(auth_token)
+      # decode & verify token without checking signature so we can extract
+      # public keys
+      public_key = nil
+      verify = false
+      decoded_tokens = JWT.decode auth_token, public_key, verify, algorithm: ALGORITHM
+      decoded_token = decoded_tokens[0]
+      puts decoded_token
+      REQUIRED_CLAIMS.each do |field|
+        raise InvalidAuthResponse.new("Missing required '#{field}' claim.") if !decoded_token.key?(field.to_s)
+      end
+      raise InvalidAuthResponse.new("Missing required 'iat' claim.") if !decoded_token["iat"]
+      raise InvalidAuthResponse.new("'iat' timestamp claim is skewed too far from present.") if (Time.now.to_i - decoded_token["iat"]).abs > VALID_WITHIN
+
+      public_keys = decoded_token['publicKeys']
+
+      raise InvalidAuthResponse.new("Invalid publicKeys array: only 1 key is supported") unless public_keys.length == 1
+
+      compressed_hex_public_key = public_keys[0]
+      bignum = OpenSSL::BN.new(compressed_hex_public_key, 16)
+      group = OpenSSL::PKey::EC::Group.new 'secp256k1'
+      public_key = OpenSSL::PKey::EC::Point.new(group, bignum)
+      ecdsa_key = OpenSSL::PKey::EC.new 'secp256k1'
+      ecdsa_key.public_key = public_key
+      verify = true
+
+      # decode & verify signature
+      decoded_tokens = JWT.decode auth_token, ecdsa_key, verify, algorithm: ALGORITHM, exp_leeway: LEEWAY
+      decoded_token = decoded_tokens[0]
+
+      raise InvalidAuthResponse.new("Public keys don't match issuer address") unless self.public_keys_match_issuer?(decoded_token)
+
+      raise InvalidAuthResponse.new("Public keys don't match owner of claimed username") unless self.public_keys_match_username?(BLOCKSTACK_API, decoded_token)
+    rescue JWT::VerificationError => error
+      raise InvalidAuthResponse.new("Signature on JWT is invalid")
+    rescue JWT::DecodeError => error
+      raise InvalidAuthResponse.new("Unable to decode JWT")
+    rescue RuntimeError => error
+      raise InvalidAuthResponse.new(error.message)
+    end
 
     def self.get_did_type(decentralized_id)
       did_parts = decentralized_id.split(':')
@@ -16,6 +65,32 @@ module Blockstack
       did_type = get_did_type(decentralized_id)
       return nil if did_type != 'btc-addr'
       decentralized_id.split(':')[2]
+    end
+
+    def self.public_keys_match_issuer?(decoded_token)
+      public_keys = decoded_token['publicKeys']
+      address_from_issuer = get_address_from_did(decoded_token['iss'])
+
+      raise 'Multiple public keys are not supported' unless public_keys.count == 1
+
+      address_from_public_keys = Bitcoin::pubkey_to_address public_keys.first
+
+      address_from_issuer == address_from_public_keys
+    end
+
+    def self.public_keys_match_username?(blockstack_api, decoded_token)
+      username = decoded_token["username"]
+      return true if username.nil?
+
+      response = Faraday.get "#{blockstack_api}/v1/names/#{username}"
+      json = JSON.parse response.body
+
+      raise "Issuer claimed username that doesn't exist" if response.status == 404
+      raise "Unable to verify issuer's claimed username" if response.status != 200
+
+      name_owning_address = json['address']
+      address_from_issuer = get_address_from_did decoded_token['iss']
+      name_owning_address == address_from_issuer
     end
 
     protected
