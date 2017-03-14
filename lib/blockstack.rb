1510784dd41b978f2fce0242bc784cbbb3fe117b
Larry Salibra <larry@appartisan.com>
2017-03-15 00:14:15 +0800

    add methods to work with dids

--- lib/blockstack.rb
+++ lib/blockstack.rb
@@ -5,11 +5,20 @@ require 'faraday'
 module Blockstack
     USER_AGENT = "blockstack-ruby #{VERSION}"
 
-    def self.test
-      faraday.get "http://localhost:6270/v1/names/larry.id"
+    def self.get_did_type(decentralized_id)
+      did_parts = decentralized_id.split(':')
+      raise 'Decentralized IDs must have 3 parts' if did_parts.length != 3
+      raise 'Decentralized IDs must start with "did"' if did_parts[0].downcase != 'did'
+      did_parts[1].downcase
     end
 
-    private
+    def self.get_address_from_did(decentralized_id)
+      did_type = get_did_type(decentralized_id)
+      return nil if did_type != 'btc-addr'
+      decentralized_id.split(':')[2]
+    end
+
+    protected
 
     def self.faraday
       connection = Faraday.new
--- spec/lib/blockstack_spec.rb
+++ spec/lib/blockstack_spec.rb
@@ -3,14 +3,36 @@ require 'bitcoin'
 
 describe Blockstack, :vcr => { :cassette_name => "blockstack", :record => :new_episodes }  do
 
+	context "getting did types" do
 
-	it "should test " do
-	    expect(Blockstack.test).to eq("hi")
+		it "should get did type from did" do
+			decentralized_id = "did:btc-addr:1EXwrz2E3WvxhdeRB1ArKtdE5NKtKC7Edx"
+			expect(Blockstack.get_did_type(decentralized_id)).to eq("btc-addr")
+		end
+
+		it "should raise RuntimeError if there aren't 3 parts" do
+			decentralized_id = "did:1EXwrz2E3WvxhdeRB1ArKtdE5NKtKC7Edx"
+			expect {
+		       Blockstack.get_did_type(decentralized_id)
+				}.to raise_error(RuntimeError)
+		end
+
+		it "should raise RuntimeError if it doesn't start with did" do
+			decentralized_id = "btc-addr:1EXwrz2E3WvxhdeRB1ArKtdE5NKtKC7Edx"
+			expect {
+					 Blockstack.get_did_type(decentralized_id)
+				}.to raise_error(RuntimeError)
+		end
+
 	end
 
-  # it "should raise an error when name is reserved" do
-	# 	expect {
-  #     Openname.get("pmarca")
-	# 	}.to raise_error(NameError)
-	# end
+	context "extracting bitcoin address from did" do
+		
+		it "should return bitcoin address given valid did" do
+			decentralized_id = "did:btc-addr:1EXwrz2E3WvxhdeRB1ArKtdE5NKtKC7Edx"
+			expect(Blockstack.get_address_from_did(decentralized_id)).to eq("1EXwrz2E3WvxhdeRB1ArKtdE5NKtKC7Edx")
+		end
+
+	end
+
 end
