e4bfcfde13e318722f4043b5905e70dd9c2fcc3e
Larry Salibra <larry@appartisan.com>
2018-05-01 18:04:18 +0800

    Fix failed merge

--- lib/blockstack.rb
+++ lib/blockstack.rb
@@ -5,7 +5,6 @@ require 'faraday'
 require 'jwtb'
 
 module Blockstack
-<<<<<<< HEAD
   class InvalidAuthResponse < StandardError; end
 
   USER_AGENT = "blockstack-ruby #{VERSION}"
@@ -14,7 +13,7 @@ module Blockstack
 
   DEFAULT_LEEWAY = 30 # seconds
   DEFAULT_VALID_WITHIN = 30 # seconds
-  DEFAULT_API = 'http://localhost:6270'
+  DEFAULT_API = 'https://core.blockstack.org'
 
   def self.api=(api)
     @api = api || DEFAULT_API
@@ -68,90 +67,6 @@ module Blockstack
 
     REQUIRED_CLAIMS.each do |field|
       fail InvalidAuthResponse.new("Missing required '#{field}' claim.") unless decoded_token.key?(field.to_s)
-=======
-    class InvalidAuthResponse < StandardError; end
-
-    USER_AGENT = "blockstack-ruby #{VERSION}"
-    ALGORITHM = "ES256K"
-    REQUIRED_CLAIMS = %w(iss iat jti exp username profile public_keys)
-
-    DEFAULT_LEEWAY = 30 # seconds
-    DEFAULT_VALID_WITHIN = 30 # seconds
-    DEFAULT_API = "https://core.blockstack.org"
-
-    def self.api=(api)
-      @@api = api.nil? ? DEFAULT_API : api
-    end
-
-    def self.api
-      @@api
-    end
-
-    def self.leeway=(leeway)
-      @@leeway = leeway.nil? ? DEFAULT_LEEWAY : leeway
-    end
-
-    def self.leeway
-      @@leeway
-    end
-
-    def self.valid_within=(valid_within)
-      @@valid_within = valid_within.nil? ? DEFAULT_VALID_WITHIN : valid_within
-    end
-
-    def self.valid_within
-      @@valid_within
-    end
-
-    def self.verify_auth_response(auth_token)
-      # decode & verify token without checking signature so we can extract
-      # public keys
-      public_key = nil
-      verify = false
-      decoded_tokens = JWTB.decode auth_token, public_key, verify, algorithm: ALGORITHM
-      decoded_token = decoded_tokens[0]
-
-      REQUIRED_CLAIMS.each do |field|
-        raise InvalidAuthResponse.new("Missing required '#{field}' claim.") if !decoded_token.key?(field.to_s)
-      end
-      raise InvalidAuthResponse.new("Missing required 'iat' claim.") if !decoded_token["iat"]
-      raise InvalidAuthResponse.new("'iat' timestamp claim is skewed too far from present.") if (Time.now.to_i - decoded_token["iat"]).abs > self.valid_within
-
-      public_keys = decoded_token['public_keys']
-
-      raise InvalidAuthResponse.new("Invalid public_keys array: only 1 key is supported") unless public_keys.length == 1
-
-      compressed_hex_public_key = public_keys[0]
-      bignum = OpenSSL::BN.new(compressed_hex_public_key, 16)
-      group = OpenSSL::PKey::EC::Group.new 'secp256k1'
-      public_key = OpenSSL::PKey::EC::Point.new(group, bignum)
-      ecdsa_key = OpenSSL::PKey::EC.new 'secp256k1'
-      ecdsa_key.public_key = public_key
-      verify = true
-
-      # decode & verify signature
-      decoded_tokens = JWTB.decode auth_token, ecdsa_key, verify, algorithm: ALGORITHM, exp_leeway: self.leeway
-      decoded_token = decoded_tokens[0]
-
-      raise InvalidAuthResponse.new("Public keys don't match issuer address") unless self.public_keys_match_issuer?(decoded_token)
-
-      raise InvalidAuthResponse.new("Public keys don't match owner of claimed username") unless self.public_keys_match_username?(decoded_token)
-
-      return decoded_token
-    rescue JWTB::VerificationError => error
-      raise InvalidAuthResponse.new("Signature on JWT is invalid")
-    rescue JWTB::DecodeError => error
-      raise InvalidAuthResponse.new("Unable to decode JWT")
-    rescue RuntimeError => error
-      raise InvalidAuthResponse.new(error.message)
-    end
-
-    def self.get_did_type(decentralized_id)
-      did_parts = decentralized_id.split(':')
-      raise 'Decentralized IDs must have 3 parts' if did_parts.length != 3
-      raise 'Decentralized IDs must start with "did"' if did_parts[0].downcase != 'did'
-      did_parts[1].downcase
->>>>>>> develop
     end
     fail InvalidAuthResponse.new("Missing required 'iat' claim.") unless decoded_token['iat']
     fail InvalidAuthResponse.new("'iat' timestamp claim is skewed too far from present.") if (Time.now.to_i - decoded_token['iat']).abs > valid_within
